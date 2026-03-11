d21a4e7f6e23d7592194840b8db08acd0aec54ca
barklin-01 <barklin2003@gmail.com>
2026-03-11 00:00:50 -0500

    feat: implement grade evaluation logic and output messages

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What ir you grade porcentaje");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+
+        string letter = "";
+
+        if (gradeInt >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeInt >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeInt >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeInt >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you failed.");
+        }
     }
 }
