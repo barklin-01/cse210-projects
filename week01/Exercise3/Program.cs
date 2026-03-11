ce6487cf4533656f2007ae13a585b36844a96945
barklin-01 <barklin2003@gmail.com>
2026-03-11 12:35:28 -0500

    nwe code

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,9 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random random = new Random();
-        int magicNumber = random.Next(1, 101);
+        Console.WriteLine("Welcome to the Magic Number Game!");
+        string gameagain;
 
-        int userGuess = -1;
+        do
+        {
+            Random random = new Random();
+            int magicnumber = random.Next(1, 101);
+
+            int adivinanza = -1;
+            while (adivinanza != magicnumber)
+            {
+                Console.Write("Guess the magic number (between 1 and 100):What is your guess? ");
+                adivinanza = int.Parse(Console.ReadLine());
+
+                if (adivinanza < magicnumber)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else if (adivinanza > magicnumber)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You've guessed the magic number!");
+                }
+            }
+            Console.Write("Do you want to play again? (yes/no): ");
+            gameagain = Console.ReadLine().ToLower();
+        } while (gameagain == "yes");
+        Console.WriteLine("Thanks for playing! Goodbye!");
     }
 }
