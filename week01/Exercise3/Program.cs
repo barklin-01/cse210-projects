acdeef483ca72137950ee02952c0726ff412b44d
barklin-01 <barklin2003@gmail.com>
2026-03-11 00:24:27 -0500

    new

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int userGuess = -1;
     }
 }
