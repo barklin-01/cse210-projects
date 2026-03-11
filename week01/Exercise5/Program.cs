be3d5cb2e2218070795943ab6836f0f093a002c4
barklin-01 <barklin2003@gmail.com>
2026-03-11 14:43:38 -0500

    new

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        message();
+        string name = askname();
+        int number = asknumber();
+        int square = squarenumber(number);
+        displayresult(name, square);
+
+    }
+    //Mensaje de bienvenida
+    static void message()
+    {
+        Console.WriteLine("Wlcome to the program");
+    }
+    // Pedir nombre al usuario
+    static string askname()
+    {
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+    //Numero favorito
+    static int asknumber()
+    {
+        Console.Write("What is your favorite number? ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+    static int squarenumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+    static void displayresult(string name,int square)
+    {
+        Console.WriteLine($"{name}, the square of your favorite number is {square}");
     }
 }
