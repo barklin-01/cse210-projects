8228b14a40f8f9d21b6c2aaee1cecffc64cea4f5
barklin-01 <barklin2003@gmail.com>
2026-04-03 21:46:04 -0500

    new

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        // Order 1 (USA)
+        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
+        Customer customer1 = new Customer("Juan Benavidez", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Kitchen", "P001", 1200, 1));
+        order1.AddProduct(new Product("Microwave", "P002", 250, 2));
+        order1.AddProduct(new Product("Blender", "P003", 150, 1));
+
+        // Order 2 (International)
+        Address address2 = new Address("Av Quito", "Otavalo", "Imbabura", "Ecuador");
+        Customer customer2 = new Customer("Noe Gonzalez", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Phone", "P004", 700, 3));
+        order2.AddProduct(new Product("Earphones", "P005", 75, 4));
+        order2.AddProduct(new Product("Laptop", "P006", 850, 1));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total: ${order1.GetTotalPrice()}");
+
+        Console.WriteLine("\n-------------------\n");
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total: ${order2.GetTotalPrice()}");
     }
 }
