c9570fc163759ae3d3d0c011899a11e8089cbd09
barklin-01 <barklin2003@gmail.com>
2026-04-10 21:09:16 -0500

    new

--- week05/Mindfulness/ActiveBreaks.cs
+++ week05/Mindfulness/ActiveBreaks.cs
@@ -1,7 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
 public class ActiveBreaks : Activity
 {
+    private List<string> _instructions = new List<string>
+    {
+        "Raise your arms and stretch up high.",
+        "Gently roll your neck in circles.",
+        "Tilt your head from side to side.",
+        "Stretch your legs forward.",
+        "Rotate your shoulders forward and backward.",
+        "Take a deep breath in and slowly exhale.",
+        "Twist your torso side to side.",
+        "Shake out your hands and wrists.",
+        "Open and close your hands repeatedly.",
+        "Stand up and touch your toes.",
+        "March in place.",
+        "Lift your knees one at a time.",
+        "Stretch one arm across your chest.",
+        "Switch arms and stretch the other side.",
+        "Roll your ankles in circles.",
+        "Stand on your tiptoes and hold.",
+        "Do a gentle back stretch.",
+        "Clasp your hands and push them forward.",
+        "Reach one arm overhead and lean to the side.",
+        "Switch sides and repeat the stretch.",
+        "Take a deep breath and hold for a few seconds.",
+        "Relax your shoulders and drop tension.",
+        "Gently shake your whole body.",
+        "Smile and relax your face muscles."
+    };
+
+    private Random _random = new Random();
+
     public ActiveBreaks()
-        : base("Active Breaks", "Esta actividad te guiará con estiramientos para relajar el cuerpo.")
+        : base("Active Breaks", "This activity encourages you to take short breaks to stretch and move, helping to refresh your mind and body.")
     {
     }
 
@@ -9,18 +44,16 @@ public class ActiveBreaks : Activity
     {
         Start();
 
-        Console.WriteLine("Levanta los brazos y estírate...");
-        Thread.Sleep(3000);
+        int numberOfExercises = 8; 
 
-        Console.WriteLine("Gira suavemente el cuello...");
-        Thread.Sleep(3000);
+        var shuffled = _instructions.OrderBy(x => _random.Next()).ToList();
 
-        Console.WriteLine("Estira las piernas...");
-        Thread.Sleep(3000);
+        foreach (var instruction in shuffled.Take(numberOfExercises))
+        {
+            CountdownInline(instruction, 5);
+            Console.WriteLine();
+        }
 
         End();
     }
 }
-
-
-
